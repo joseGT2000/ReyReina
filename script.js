db58e404d0d85410c5922e46505679ab2fdabb28
joseGT2000 <74601326+joseGT2000@users.noreply.github.com>
2026-03-15 16:21:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,8 +37,10 @@ function updateRope(applyPhysics = true) {
   const nx = -dy / dist;
   const ny = dx / dist;
 
-  const cx = midX + nx * curveAmount;
-  const cy = midY + ny * curveAmount;
+  const sag = Math.min(200, dist * 0.4);
+
+  const cx = midX;
+  const cy = midY + sag;
 
   rope.setAttribute("d", `M ${kc.x} ${kc.y} Q ${cx} ${cy} ${qc.x} ${qc.y}`);
 
@@ -112,6 +114,9 @@ function applyPositions(data) {
 
   queen.style.left = data.queen.left + "px";
   queen.style.top = data.queen.top + "px";
+
+  king.style.visibility = "visible";
+  queen.style.visibility = "visible";
 
   updateRope(false);
 }
